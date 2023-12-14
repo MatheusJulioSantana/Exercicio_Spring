32f7a9174481cabb135d595b0c9f15a045964f9a
Matheus <mattzeppelin.mj@gmail.com>
2023-12-14 13:23:18 -0300

    Estrutura do projeto

---
+++ src/main/java/com/greenvapor/GreenvaporApplication.java
@@ -0,0 +1,13 @@
+package com.greenvapor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenvaporApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenvaporApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greenvapor/GreenvaporApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenvapor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenvaporApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
