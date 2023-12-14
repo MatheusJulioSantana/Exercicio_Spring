7128584a9c4320f89c7e966fd03030432c42fe01
Matheus <mattzeppelin.mj@gmail.com>
2023-12-14 14:44:22 -0300

    Crud categoria

---
+++ src/main/java/com/greenvapor/model/Categoria.java
@@ -0,0 +1,46 @@
+package com.greenvapor.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "É obrigatório inserir a categoria")
+	private String nome;
+	
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+}
+
---
+++ src/main/java/com/greenvapor/repository/CategoriaRepository.java
@@ -0,0 +1,14 @@
+package com.greenvapor.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.greenvapor.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository <Categoria, Long> {
+
+	public List<Categoria> findAllByNomeContainingIgnoreCase(@Param("nome")String nome);
+	}
+	
